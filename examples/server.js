41b5239f18a9e1b9517d17cdb31a37ef4b0c1678
ma <442026460@qq.com>
2020-12-18 18:01:33 +0800

    处理header参数

--- examples/server.js
+++ examples/server.js
@@ -9,35 +9,6 @@ const router = express.Router()
 const app = express()
 const compiler = webpack(WebpackConfig)
 
-router.get('/simple/get', function(req, res) {
-  res.json({
-    msg: `hello world`
-  })
-})
-
-router.get('/base/get', function(req, res) {
-  res.json(req.query)
-})
-
-router.post('/base/post', function(req, res) {
-  res.json(req.body)
-})
-
-router.post('/base/buffer', function(req, res) {
-  let msg = []
-  req.on('data', chunk => {
-    if (chunk) {
-      msg.push(chunk)
-    }
-  })
-  req.on('end', () => {
-    let buf = Buffer.concat(msg)
-    res.json(buf.toJSON())
-  })
-})
-
-app.use(router)
-
 app.use(
   webpackDevMiddleware(compiler, {
     publicPath: '/__build__/',
@@ -54,6 +25,36 @@ app.use(express.static(__dirname))
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(router)
+
+router.get('/simple/get', function(req, res) {
+  res.json({
+    msg: `hello world`
+  })
+})
+
+router.get('/base/get', function(req, res) {
+  res.json(req.query)
+})
+
+router.post('/base/post', function(req, res) {
+  console.log(req.body)
+  res.json(req.body)
+})
+
+router.post('/base/buffer', function(req, res) {
+  let msg = []
+  req.on('data', chunk => {
+    if (chunk) {
+      msg.push(chunk)
+    }
+  })
+  req.on('end', () => {
+    let buf = Buffer.concat(msg)
+    res.json(buf.toJSON())
+  })
+})
 
 const port = process.env.PORT || 8080
 module.exports = app.listen(port, () => {
